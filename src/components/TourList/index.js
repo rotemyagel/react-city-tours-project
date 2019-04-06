0d6c068a0587a168ec11ef66f98115179c8dcc9f
rotemyagel <rotemyagel@gmail.com>
2019-04-06 19:57:12 +0300

    tour city finish

---
+++ src/components/TourList/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import Tour from '../Tour'
+import './tourlist.scss'
+import {tourData} from '../../tourData'
+
+
+class TourList extends Component {
+
+    state = {
+        tours: tourData
+    }
+
+    removeTour = (id) => {
+        const{tours} = this.state;
+        const sortedTours = tours.filter(tour=>tour.id !== id)
+        this.setState({
+            tours: sortedTours
+        })
+
+
+    }
+
+    render () {
+
+        const {tours} = this.state
+
+        // console.log(this.state.tours)
+        return (
+            <section className="tourlist">
+               {
+                   tours.map((tour)=>{
+                    return (
+                        <Tour key={tour.id} tour={tour} removeTour={this.removeTour}/>
+                    )
+                   })
+               }
+            </section>
+        )
+    }
+}
+
+export default TourList
